2b48154e9b95c778d75d1466ceeb306a5e43cfef
21bca34 <96609865+21bca34@users.noreply.github.com>
2022-03-24 11:27:31 +0530

    Add files via upload
    error

---
+++ String/soluion16.py
@@ -0,0 +1,22 @@
+# write a script to enter five string in a list and check and print string whose length has even number of character. without any string function.
+
+def createlist():
+    l=[]
+    for i in  range(5):
+        s=input("Enter string")
+        l.append(s)
+    return(l)
+a=createlist()
+for i in a:
+    print(l)
+
+def evenslength():
+    count=-
+    for i is k:
+        if k%2==0:
+            count+=i
+        return(k)
+s=evenslenghth()
+print(k)
+
+    
